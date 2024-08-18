ea59132e86919b0e07a50267f5d0d5449a436fff
lifanjie <lifanjiemail@163.com>
2024-08-18 22:50:18 +0800

    [glog] build is ok

--- src/control/src/control_main.cpp
+++ src/control/src/control_main.cpp
@@ -1,8 +1,12 @@
 // Copyright
 #include <memory>
+#include "glog/logging.h"
 #include "control/control_node.hpp"
 
 int main(int argc, char * argv[]) {
+  // init glog
+  google::InitGoogleLogging(argv[0]);
+  LOG(ERROR) << "Hello, World!";
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<control::ControlNode>());
   rclcpp::shutdown();
